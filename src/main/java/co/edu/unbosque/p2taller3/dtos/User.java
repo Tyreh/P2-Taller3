16b54f9727d081e6e4885c0a416864d712c9c432
Tyreh <oscar7u72@gmail.com>
2022-03-27 11:28:20 -0500

    Initial commit

---
+++ src/main/java/co/edu/unbosque/p2taller3/dtos/User.java
@@ -0,0 +1,45 @@
+package co.edu.unbosque.p2taller3.dtos;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class User {
+
+    @CsvBindByName
+    private String username;
+
+    @CsvBindByName
+    private String password;
+
+    @CsvBindByName
+    private String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/co/edu/unbosque/p2taller3/services/UserService.java
@@ -0,0 +1,11 @@
+package co.edu.unbosque.p2taller3.services;
+
+import co.edu.unbosque.p2taller3.dtos.User;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+
+}
